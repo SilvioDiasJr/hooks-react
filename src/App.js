d18c874e36ea455574b696941787c3e23193dd4f
Silvio <silvio.dias@etec.sp.gov.br>
2021-01-17 02:49:38 -0300

    adicionar o componente Hook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hooks from './components/Hooks'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hooks />
+    </div>
+  );
+}
+
+export default App;
